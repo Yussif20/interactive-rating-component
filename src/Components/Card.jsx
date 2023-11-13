c7bf6ad0d323788892bd9582d345809d23b98558
Yousef <yousefayman2000500@gmail.com>
2023-11-13 11:34:37 +0200

    adding the rating list

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -3,15 +3,29 @@ import { AiFillStar } from "react-icons/ai";
 const Card = () => {
   return (
     <div className="w-1/4 p-7 bg-[--Dark-Blue] rounded-lg flex gap-6 flex-col">
-      <AiFillStar className="text-[--Orange]" />
+      <AiFillStar className=" bg-[--light-Blue] rounded-full text-[--Orange] " />
       <h1 className="text-4xl">How did we do?</h1>
       <p className="text-[--Light-Grey]">
         Please let us know how we did with your support request.All feedback is
         appreciated to help us improve improve our offering!
       </p>
-      {Array(5).map((rate) => {
-        return <li key={rate}>1</li>;
-      })}
+      <ul className="flex gap-3">
+        <li className="bg-[--light-Blue] p-4 rounded-full">
+          <button>1</button>
+        </li>
+        <li className="bg-[--light-Blue] p-4 rounded-full">
+          <button>2</button>
+        </li>
+        <li className="bg-[--light-Blue] p-4 rounded-full">
+          <button>3</button>
+        </li>
+        <li className="bg-[--light-Blue] p-4 rounded-full">
+          <button>4</button>
+        </li>
+        <li className="bg-[--light-Blue] p-4 rounded-full">
+          <button>5</button>
+        </li>
+      </ul>
       <button className="bg-[--Orange] rounded-full w-full  p-3">SUBMIT</button>
     </div>
   );
