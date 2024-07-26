f591736babce9abe6646240a7b409426c777e8dd
Yousef ayman <yousefayman2000500@gmail.com>
2024-07-26 07:05:51 +0300

    adding the submiting function to the button

--- script.js
+++ script.js
@@ -5,6 +5,18 @@ const ratingNumbers = document.querySelectorAll(".rating__num");
         if(activeEl){
             activeEl.classList.remove("active")
         }
-        num.classList.add("active")
+        num.classList.add("active");
     })
 })
+const ratingSubmit = ()=>{
+    const activeEl = document.querySelector(".active");
+    if(activeEl){
+        const rating = activeEl.innerText
+        document.querySelector(".card--subtext").innerText = `You selected ${rating} out of 5`
+        document.querySelector(".card__info-start").classList.add("submitted")
+    }else{
+        document.querySelector(".warning").innerText =`Please choose a rating`
+    }
+}
+
+document.querySelector(".cta-btn").addEventListener("click", ratingSubmit)
