e7a1b2848025fb5e26054ed49226723b8813c85e
Yousef ayman <yousefayman2000500@gmail.com>
2024-07-26 06:37:09 +0300

    enabling the user to choose a rating

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log("HELOOOOOOOOOOOOO")
+const ratingNumbers = document.querySelectorAll(".rating__num");
+[...ratingNumbers].map((num)=>{
+    num.addEventListener("click",()=>{
+        const activeEl = document.querySelector(".active");
+        if(activeEl){
+            activeEl.classList.remove("active")
+        }
+        num.classList.add("active")
+    })
+})
